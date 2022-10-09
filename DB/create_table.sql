8546f49a3844d9ab2c37f1bf051712b0cc1a8b83
scott3263 <scott00@naver.com>
2022-10-09 23:27:19 +0900

    table create sql 수정

--- DB/create_table.sql
+++ DB/create_table.sql
@@ -2,7 +2,7 @@ drop table `Collatz_Conjecture`;
 
 CREATE TABLE `Collatz_Conjecture` (
   `seq` double NOT NULL AUTO_INCREMENT COMMENT '순번',
-  `bounce` long NOT NULL COMMENT '연산횟수',
+  `bounce` double NOT NULL COMMENT '연산횟수',
   `max_num` double NOT NULL COMMENT '최대값',
   `hash_value` varchar(200) NOT NULL COMMENT '해시값',
   `reg_date` datetime DEFAULT current_timestamp() COMMENT '등록일시',
@@ -12,3 +12,19 @@ CREATE TABLE `Collatz_Conjecture` (
   PRIMARY KEY (`seq`),
   UNIQUE KEY `collatz_conjecture_UNIQUE_01`(`hash_value`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Collatz Conjecture';
+
+
+select * from Collatz_Conjecture;
+
+select * from Collatz_Conjecture order by reg_date desc;
+
+select max(seq) , max(bounce), max(max_num), max(reg_date) from Collatz_Conjecture;
+
+select * from Collatz_Conjecture where max_num = 1504774;
+
+select * from Collatz_Conjecture where bounce = (select max(bounce) from Collatz_Conjecture where seq <= 10000);
+
+
+
+
+select * from 
